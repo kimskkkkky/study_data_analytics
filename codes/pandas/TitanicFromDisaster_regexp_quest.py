c14022872fc0e35db430d9989f4b064a0d52d39c
kimskkkkky <ses513a@gmail.com>
2023-08-22 15:57:45 +0900

    sky

--- codes/pandas/TitanicFromDisaster_regexp_quest.py
+++ codes/pandas/TitanicFromDisaster_regexp_quest.py
@@ -5,9 +5,13 @@ data = df_TFD['Name']
 df_TFD_name = pd.DataFrame(data)
 print(df_TFD_name)
 
+# 성 뽑아낸 후 컬럼 생성
+
 pattern = r'^([A-Za-z]+)'
 df_TFD_name['firstname'] = df_TFD_name['Name'].str.extract(pattern)
 print(df_TFD_name)
+
+# 결혼한 사람만 뽑아낸 후 컬럼 생성
 
 pattern = r'(Mrs?)'
 df_TFD_name['marry'] = df_TFD_name['Name'].str.extract(pattern)
